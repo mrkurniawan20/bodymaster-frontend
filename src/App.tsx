b5ad8944f89e0adcb03407f97c567eaa1a62426b
NewUser <mraflikurniawan20@gmail.com>
2025-05-26 20:30:53 +0700

    fix: fixing MemberList mapped fetched data to be put into conditional rendering with !loading condition fixing member expired to not use context, rather than to fetch its own data to save fetching when loading admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import './App.css';
-// import MemberInfo from './routes/user/MemberInfo';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-    // // <BrowserRouter>
-    // //   <Routes >
-    // //     <Route path="/" Component={GymLogin}  />
-    // //     <Route path="/landingPage" Component={MemberLandingPage} />
-    // //     <Route path="/memberInfo" Component={MemberInfo} />
-    // //     <Route path="/addMember" Component={AddMember} />
-    // //     <Route path="/dashboard" Component={AdminDashboard} />
-    // //     <Route path="/list" Component={MemberListDummy} />
-    // //     <Route path="/visitorLog" Component={VisitorLog} />
-    // //     <Route path="/payment" Component={PaymentPage} />
-    // //   </Routes>
-    // // </BrowserRouter>
-    // <>
-    //   <AdminDashboard />
-    //   <AddMember />
-    //   <GymLogin />
-    //   <MemberInfo />
-    //   <MemberListDummy />
-    // </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
